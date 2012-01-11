6f17d168f9285a0b2d545cb94fb7568f40797611
Lasse Pedersen <lass5915@hotmail.com>
2012-01-11 16:58:29 +0100

    mathias

---
+++ derp shotdown.bat
@@ -0,0 +1,16 @@
+@echo off
+cls
+echo Hacking facebook
+PING 1.1.1.1 -n 1 -w 5000 >NUL
+echo .
+PING 1.1.1.1 -n 1 -w 5000 >NUL
+echo .
+PING 1.1.1.1 -n 1 -w 5000 >NUL
+echo .
+PING 1.1.1.1 -n 1 -w 5000 >NUL
+echo færdig fjerner og sender nu alle data til main server og lukker ned
+ping 1.1.1.1 -n 1 -w 8000 >NUL
+pause
+shutdown /p >nul
+
+
