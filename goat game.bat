3653e63b1ac79f2794a8a7aec71c302b1717f2c0
Lasse Pedersen <lass5915@hotmail.com>
2012-01-11 17:30:02 +0100

    fixed somthin wich i don't remember

--- goat game.bat
+++ goat game.bat
@@ -18,11 +18,17 @@ echo door 2
 ping 1.1.1.1.1 -n 1 -w 1000 >nul
 echo door 3
 ping 1.1.1.1.1 -n 1 -w 10000 >nul
+
+goto gen
+:choice
+
 echo choose now.....
+echo %door%
 choice /c:123 /n /m "door:"
-IF ERRORLEVEL 3 goto door3 
-IF ERRORLEVEL 2 goto door2
-IF ERRORLEVEL 1 goto door1
+
+IF ERRORLEVEL == %door% goto win 
+else goto lose
+
 
 :door1
 echo door 1
@@ -36,5 +42,42 @@ goto end
 echo door 3
 pause
 goto end
+
 :end
 exit
+
+:win
+echo you win!
+ping 1.1.1.1.1 -n 1 -w 500000 >nul
+pause
+
+
+:lose
+echo you DERP!
+ping 1.1.1.1.1 -n 1 -w 500000 >nul
+pause
+
+
+:gen
+Setlocal EnableDelayedExpansion
+Set _RNDLength=1
+Set _Alphanumeric=123
+Set _Str=%_Alphanumeric%987654321
+:_LenLoop
+IF NOT "%_Str:~18%"=="" SET _Str=%_Str:~9%& SET /A _Len+=9& GOTO :_LenLoop
+SET _tmp=%_Str:~9,1%
+SET /A _Len=_Len+_tmp
+Set _count=0
+SET door=
+:_loop
+Set /a _count+=1
+SET _RND=%Random%
+Set /A _RND=_RND%%%_Len%
+SET door=!door!!_Alphanumeric:~%_RND%,1!
+If !_count! lss %_RNDLength% goto _loop
+goto choice
+
+
+
+
+IF NOT ERRORLEVEL == %door% goto lose
